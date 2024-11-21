f149ad47d55c60a2e9903f7c6ff1180dd8cba9b9
khrishchanovich <orannon@gmail.com>
2024-11-22 00:00:25 +0300

    feat: add elements to select type of registration

--- VolunteerAssistance/app/src/main/java/com/example/volunteerassistance/PreRegistrationActivity.kt
+++ VolunteerAssistance/app/src/main/java/com/example/volunteerassistance/PreRegistrationActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.volunteerassistance.ui.theme.State
 import com.example.volunteerassistance.ui.theme.VolunteerAssistanceTheme
 
@@ -27,7 +38,58 @@ class PreRegistrationActivity : ComponentActivity() {
 
 @Composable
 fun PreRegistrationScreen() {
-    Text("Добро пожаловать на экран регистрации!")
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Bottom),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+            ) {
+                Text(
+                    "Регистрация при помощи почты",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center)
+            }
+
+            Button (
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+            ) {
+                Text(
+                    "Регистрация при помощи Google",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center)
+            }
+
+            Button (
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+            ) {
+                Text(
+                    "Регистрация при помощи Yandex",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
