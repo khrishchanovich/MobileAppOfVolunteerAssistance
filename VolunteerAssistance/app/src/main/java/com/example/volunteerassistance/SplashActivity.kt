eaf0efe86b8d8d452024c08da991ef44eee80d95
khrishchanovich <orannon@gmail.com>
2024-12-21 14:20:35 +0300

    feat: add textToSpeach for a firs launch

--- VolunteerAssistance/app/src/main/java/com/example/volunteerassistance/SplashActivity.kt
+++ VolunteerAssistance/app/src/main/java/com/example/volunteerassistance/SplashActivity.kt
@@ -1,20 +1,29 @@
 package com.example.volunteerassistance
 
-
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.content.SharedPreferences
 import com.google.firebase.auth.FirebaseAuth
 
-
 class SplashActivity : ComponentActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences("AppPreferences", MODE_PRIVATE)
+        val isFirstLaunch = sharedPreferences.getBoolean("isFirstLaunch", true)
 
         val auth = FirebaseAuth.getInstance()
         val currentUser = auth.currentUser
 
-        if (currentUser != null) {
+        if (isFirstLaunch) {
+            sharedPreferences.edit().putBoolean("isFirstLaunch", false).apply()
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+        } else if (currentUser != null) {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         } else {
--- VolunteerAssistance/app/src/main/java/com/example/volunteerassistance/WelcomeActivity.kt
+++ VolunteerAssistance/app/src/main/java/com/example/volunteerassistance/WelcomeActivity.kt
@@ -1,4 +1,97 @@
-package com.example.volunteerassistance.ui
+package com.example.volunteerassistance
 
-class WelcomeActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.volunteerassistance.ui.theme.VolunteerAssistanceTheme
+import java.util.Locale
+
+class WelcomeActivity : ComponentActivity() {
+
+    private lateinit var textToSpeech: TextToSpeech
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        textToSpeech = TextToSpeech(this) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                textToSpeech.language = Locale("ru", "RU")
+
+                val utteranceId = "WelcomeMessageId" // Уникальный ID
+                textToSpeech.speak(
+                    "Здравствуйте! Вы находитесь в приложении 'Помоги мне!'. Нажмите на красную кнопку, если Вам необходима помощь." +
+                            "Далее на каждой странице будет находиться красный элемент - при нажатии на него Вы будете получать подсказку. Удачи!",
+                    TextToSpeech.QUEUE_FLUSH,
+                    null,
+                    utteranceId
+                )
+
+                textToSpeech.setOnUtteranceProgressListener(object : android.speech.tts.UtteranceProgressListener() {
+                    override fun onStart(utteranceId: String?) {
+                        // Действия при старте речи (если нужно)
+                    }
+
+                    override fun onDone(utteranceId: String?) {
+                        if (utteranceId == "WelcomeMessageId") { // Проверяем ID
+                            val intent = Intent(this@WelcomeActivity, StartActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+
+                    override fun onError(utteranceId: String?) {
+                    }
+                })
+            }
+        }
+
+
+        setContent {
+            VolunteerAssistanceTheme {
+                AudioScreen()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        if (::textToSpeech.isInitialized) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        super.onDestroy()
+    }
 }
+
+@Composable
+fun AudioScreen() {
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "🔊",
+                fontSize = 100.sp,
+                color = Color.Black
+            )
+        }
+    }
+}
+
